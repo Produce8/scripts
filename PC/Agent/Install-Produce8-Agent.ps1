dc9642c34adad9d444651e515b826e813751ecf0
geoff affleck <geoff.affleck@produce8.com>
2025-05-06 16:04:17 -0700

    adding download

--- PC/Agent/Install-Produce8-Agent.ps1
+++ PC/Agent/Install-Produce8-Agent.ps1
@@ -1,2 +1,4 @@
-#Download
-start-process msiexec -argumentlist "/i Produce8-Agent-1.0.585.msi /quiet ACCOUNTID=###" -wait
+#Download the msi
+Invoke-WebRequest https://desktop-agent-assets-main.s3.us-west-2.amazonaws.com/main/msi/x64/Produce8-Agent-latest.msi -OutFile c:\tmp\produce8.msi 
+#Run it with the right accountId 
+Start-Process -Wait msiexec -argumentlist "/i C:\tmp\produce8.msi /quiet ACCOUNTID=59079b49-772c-453b-bb33-70a04e372466" 
