b585f68184422dd9b8b0fa1074e9974b51a2ea91
Geoff Affleck <80722585+gaffleckp8@users.noreply.github.com>
2025-05-07 09:36:43 -0700

    Update Install-Produce8-Agent.ps1
    missing closing quote

--- PC/Agent/Install-Produce8-Agent.ps1
+++ PC/Agent/Install-Produce8-Agent.ps1
@@ -1,4 +1,4 @@
 #Download the msi
 Invoke-WebRequest https://desktop-agent-assets-main.s3.us-west-2.amazonaws.com/main/msi/x64/Produce8-Agent-latest.msi -OutFile c:\tmp\produce8.msi 
 #Run it with the right accountId 
-Start-Process -Wait msiexec -argumentlist "/i C:\tmp\produce8.msi /quiet ACCOUNTID=###
+Start-Process -Wait msiexec -argumentlist "/i C:\tmp\produce8.msi /quiet ACCOUNTID=###"
