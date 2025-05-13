3fb5c14e0a8d92d6046e193fe8212b5237aaee8e
geoff affleck <geoff.affleck@produce8.com>
2025-05-13 16:34:32 -0700

    hash account id

--- PC/Agent/Install-Produce8-Agent.ps1
+++ PC/Agent/Install-Produce8-Agent.ps1
@@ -14,4 +14,4 @@ $path = $tempDir+'\produce8-agent-latest.msi'
 Invoke-WebRequest https://desktop-agent-assets-main.s3.us-west-2.amazonaws.com/main/msi/x64/Produce8-Agent-latest.msi -OutFile $path 
 #Run it with the right accountId 
 #ie. Start-Process -Wait msiexec -argumentlist "/i C:\tmp\produce8.msi /quiet ACCOUNTID=59079b49-772c-453b-bb33-70a04e372466"
-Start-Process -Wait msiexec -argumentlist "/i $path /quiet ACCOUNTID=59079b49-772c-453b-bb33-70a04e372466"
+Start-Process -Wait msiexec -argumentlist "/i $path /quiet ACCOUNTID=###"
