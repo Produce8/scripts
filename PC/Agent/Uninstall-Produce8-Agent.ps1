97e3c2d7623e1a63d5ec5e7acb1a7ae862d91f3a
mthai-mbp <mthai@mthai-MBP.local>
2025-05-15 11:20:36 -0700

    Add check for package

--- PC/Agent/Uninstall-Produce8-Agent.ps1
+++ PC/Agent/Uninstall-Produce8-Agent.ps1
@@ -6,5 +6,10 @@ if (Get-Process -Name $processName -ErrorAction SilentlyContinue) {
     Write-Host "Process '$processName' is not running. Continue to uninstall."
 }
 
-Uninstall-Package -Name $processName -Force -Verbose
-Write-Host "$processName has been uninstalled."
+if (Get-Package -Name $processName) {
+    Uninstall-Package -Name $processName -Force -Verbose
+    Write-Host "$processName has been uninstalled successfully."
+} else {
+    Write-Host "Package '$processName' cannot be found."
+}
+
