439a0e7106b52ff3378c7c7967432dfddfa03886
mthai-mbp <mthai@mikemac0.local>
2025-08-21 10:34:30 -0700

    Download installer and install

--- Mac/Agent/install-produce8-agent.sh
+++ Mac/Agent/install-produce8-agent.sh
@@ -1,11 +1,23 @@
-# pkill "Produce8-Agent"
-pkgFilePath="{path of PKG file}"
+#!/bin/bash
 
-echo "PKG file starting."
+# Temporary path to save the installer pkg file
+PKG_PATH="/tmp/Produce8-Agent-latest.pkg"
 
-# Install the .pkg file using the installer command
-sudo installer -pkg "$pkgFilePath" -target /Applications -dumplog
+# Download the pkg
+curl -L "https://desktop-agent-assets-main.s3.us-west-2.amazonaws.com/main/pkg/arm64/Produce8-Agent-latest.pkg" -o "$PKG_PATH"
 
-echo "PKG file installed."
+if [[ $? -ne 0 ]]; then
+  echo "Unable to download the Produce8 agent installer file"
+  exit 1
+fi
 
-open -n /Applications/Produce8-Agent.app 
+# Run the installer for the system - requires elevated permissions
+echo "Installing Produce8 Agent.."
+sudo installer -pkg "$PKG_PATH" -target /
+
+if [[ $? -eq 0 ]]; then
+  echo "Produce8-Agent installed successfully"
+else
+  echo "Installation failed"
+  exit 1
+fi
