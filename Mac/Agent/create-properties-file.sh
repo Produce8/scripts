73ae3ff5de984f815635a042e712a111ab6d90de
mthai-mbp <mthai@mikemac0.local>
2025-08-21 12:36:30 -0700

    Remove redundant scripts

--- Mac/Agent/create-properties-file.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-accountId=###
-configFileDir="/Users/Shared/Produce8-Agent"
-
-mkdir $configFileDir
-
-echo "account.accountId=$accountId" > "$configFileDir/account.properties"
--- Mac/Agent/end-running-agent.sh
+++
@@ -1,8 +0,0 @@
-process_name="Produce8-Agent"
-
-killall "$process_name"
-if [ $? -eq 0 ]; then
-  echo "Process(es) $process_name terminated successfully"
-else
-  echo "Failed to terminate process(es) $process_name"
-fi
