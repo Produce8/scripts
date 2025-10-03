a40302051aa41ef94c79c42fbf85fd4db78bd538
mthai-mbp <mthai@mikemac0.local>
2025-10-02 17:11:04 -0700

    Clean up agent scripts

--- PC/Agent/Uninstall-Produce8-Agent.ps1
+++ PC/Agent/Uninstall-Produce8-Agent.ps1
@@ -1,15 +1,17 @@
 $processName = "Produce8-Agent"
 if (Get-Process -Name $processName -ErrorAction SilentlyContinue) {
-    Stop-Process -Name $processName
-    Write-Host "Process '$processName' stopped."
+    Stop-Process -Name $processName -Force
+    Write-Output "Process '$processName' stopped."
 } else {
-    Write-Host "Process '$processName' is not running. Continue to uninstall."
+    Write-Output "Process '$processName' is not running. Continue to uninstall."
 }
 
 if (Get-Package -Name $processName) {
-    Uninstall-Package -Name $processName -Force -Verbose
-    Write-Host "$processName has been uninstalled successfully."
+    if (Uninstall-Package -Name $processName -Force -Verbose) {
+        Write-Output "$processName has been uninstalled successfully."
+    } else {
+        Write-Output "Failed to uninstall '$processName'."
+    }
 } else {
-    Write-Host "Package '$processName' cannot be found."
+    Write-Output "Package '$processName' cannot be found."
 }
-
