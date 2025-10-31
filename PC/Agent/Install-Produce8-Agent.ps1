65df2a7385d3789e14c4a42c84b7594c1976c602
mthai-mbp <mthai@mikethaimac-123.local>
2025-10-31 12:37:22 -0700

    Add alt download links for diff architectures

--- PC/Agent/Install-Produce8-Agent.ps1
+++ PC/Agent/Install-Produce8-Agent.ps1
@@ -1,3 +1,8 @@
+# This script will
+#   1. Create a temporary directory 
+#   2. Download the installer from AWS S3 to the temp directory
+#   3. Run the installer from the temp directory
+
 $tempDir = ""
 
 if (Test-Path -Path 'C:\tmp'){
@@ -6,6 +11,7 @@ if (Test-Path -Path 'C:\tmp'){
 elseif (Test-Path -Path 'C:\temp'){
     $tempDir = "C:\temp"
 } else {
+    # Create the temp folder if it does not exist
     $tempDir = 'C:\tmp'
     New-Item -Path $tempDir -ItemType Directory -Force | Out-Null
 }
@@ -13,6 +19,10 @@ elseif (Test-Path -Path 'C:\temp'){
 $path = $tempDir+'\produce8-agent-latest.msi'
 
 Invoke-WebRequest https://desktop-agent-assets-main.s3.us-west-2.amazonaws.com/main/msi/x64/Produce8-Agent-latest.msi -OutFile $path 
-# Run it with the right accountId 
+
+# For Arm64 devices use this link instead
+#Invoke-WebRequest https://desktop-agent-assets-main.s3.us-west-2.amazonaws.com/main/msi/arm64/Produce8-Agent-latest.msi -OutFile $path 
+
+# Set your account id by replacing # in the next line.
 # ie. Start-Process -Wait msiexec -argumentlist "/i C:\tmp\produce8.msi /quiet ACCOUNTID=59079b49-772c-453b-bb33-70a04e372466"
-Start-Process -Wait msiexec -argumentlist "/i $path /quiet ACCOUNTID=###"
+Start-Process -Wait msiexec -argumentlist "/i $path /quiet ACCOUNTID=#"
