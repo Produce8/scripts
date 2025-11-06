950cd3f6e722f4c8193c500172abe29436901df1
mthai-mbp <mthai@mtMac.local>
2025-11-05 17:04:02 -0800

    Add option alternative architecture link

--- PC/DesktopApp/Install-Produce8-App.ps1
+++ PC/DesktopApp/Install-Produce8-App.ps1
@@ -1,9 +1,9 @@
 ## Install the Produce8 Desktop App
-#Download the msi - ARM64 devices
-Invoke-WebRequest https://p8desktopapp-v2-main.s3.us-west-2.amazonaws.com/%40cyclops/desktopapp/main/win32/arm64/Produce8.msi -OutFile c:\tmp\produce8-app.msi 
+#Download the msi - x64 devices
+Invoke-WebRequest https://p8desktopapp-v2-main.s3.us-west-2.amazonaws.com/%40cyclops/desktopapp/main/win32/x64/Produce8.msi -OutFile c:\tmp\produce8-app.msi 
 
-#For X64 devices, the download link is slightly different
-#Invoke-WebRequest https://p8desktopapp-v2-main.s3.us-west-2.amazonaws.com/%40cyclops/desktopapp/main/win32/x64/Produce8.msi -OutFile c:\tmp\produce8-app.msi 
+#For ARM64 devices, the download link is slightly different
+#Invoke-WebRequest https://p8desktopapp-v2-main.s3.us-west-2.amazonaws.com/%40cyclops/desktopapp/main/win32/arm64/Produce8.msi -OutFile c:\tmp\produce8-app.msi 
 
 #run it
 Start-Process -Wait msiexec -argumentlist "/i C:\tmp\produce8-app.msi"
